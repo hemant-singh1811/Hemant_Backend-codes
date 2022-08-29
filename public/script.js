9f56b96e870b544df1fe043e6304102a1e749f14
HEMANT SINGH <gurpreetmarok@Hemant-MacBook-Pro.local>
2022-08-30 05:10:34 +0530

    fivecommit

--- public/script.js
+++ public/script.js
@@ -1,2 +1,5 @@
 let socket=io();
 
+socket.on("newuser",(data)=>{
+    console.log("server socket id: ",data.id);
+})
--- server.js
+++ server.js
@@ -5,20 +5,15 @@ const server=http.createServer(app);
 const socket=require('socket.io');
 const io=socket(server);
 
-
-
 let Port=process.env.PORT  || 5000
-
 
 app.use("/chat",express.static(__dirname+'/public'))
 
 app.get("/",(req,res)=>{
-
    res.json({
     name:"hemant",
     designation:"IT"
    })
-
 })
 
 app.post('/',(req,res)=>{
@@ -27,6 +22,9 @@ app.post('/',(req,res)=>{
 
 io.on("connection",(socket)=>{
     console.log("new connection :",socket.id);
+    socket.emit("newuser",{
+        id:socket.id;
+    });
 })
 
 server.listen(Port,()=>{
