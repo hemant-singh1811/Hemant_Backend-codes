11863a54b07163583e1ca84ca2e6ed7bd72a214b
HEMANT SINGH <gurpreetmarok@Hemant-MacBook-Pro.local>
2022-08-30 04:59:37 +0530

    tcommit

--- server.js
+++ server.js
@@ -5,7 +5,14 @@ const app=express();
 let Port=process.env.PORT  || 5000
 
 
-app.use(express.static(__dirname+'/public'))
+// app.use(express.static(__dirname+'/public'))
+
+app.get("/",(req,res)=>{
+   res.json({
+    name:"hemant",
+    designation:"IT"
+   })
+})
 
 app.listen(Port,()=>{
     console.log("server is running",Port);
