189235c2944b4becf9ead5102eeb61c2fc9ab2dd
HEMANT SINGH <gurpreetmarok@Hemant-MacBook-Pro.local>
2022-08-30 05:08:33 +0530

    fcommit

--- server.js
+++ server.js
@@ -1,19 +1,34 @@
 const express=require("express")
-
 const app=express();
+const http=require("http");
+const server=http.createServer(app);
+const socket=require('socket.io');
+const io=socket(server);
+
+
 
 let Port=process.env.PORT  || 5000
 
 
-// app.use(express.static(__dirname+'/public'))
+app.use("/chat",express.static(__dirname+'/public'))
 
 app.get("/",(req,res)=>{
+
    res.json({
     name:"hemant",
     designation:"IT"
    })
+
 })
 
-app.listen(Port,()=>{
+app.post('/',(req,res)=>{
+   res.send("post data")
+})
+
+io.on("connection",(socket)=>{
+    console.log("new connection :",socket.id);
+})
+
+server.listen(Port,()=>{
     console.log("server is running",Port);
 })
