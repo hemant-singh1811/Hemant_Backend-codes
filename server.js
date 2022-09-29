fcdbf35f828c517b9855e88d2b1057f48a5c9058
hemant-singh1811 <hemantsingh1852@gmail.com>
2022-09-30 02:50:33 +0530

    socket

--- server.js
+++ server.js
@@ -227,7 +227,7 @@ io.on("connection", async (socket) => {
         }
     });
 
-    socket.on("disconnected",async(socket)=>{
+    socket.on("disconnect", async() =>{
         console.log('disconnected : ',socket.id);
     })
 
